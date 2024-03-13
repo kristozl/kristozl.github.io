5e25ad2abee89227bfff82a33f85e5373f7f6287
kristozl <kristozl@users.noreply.github.com>
2024-03-13 10:55:36 +0100

    Update download.js

--- download.js
+++ download.js
@@ -1,1 +1,21 @@
+function downloadURI(uri, name) {
+	console.log("Descargando. Esta operación puede tardar varios segundos...");
+    fetch(uri)
+        .then(resp => resp.blob())
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.style.display = "none";
+            a.href = url;
+            a.download = name;
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+            console.log("end");
+        })
+        .catch(function(){
+        	console.log("error");
+        });
+}
 
+downloadURI("script.php","excelname.xls");
